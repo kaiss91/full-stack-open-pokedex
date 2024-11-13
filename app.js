e03600c6e34e50c0125c7cfa4931cbce4cf8dfc1
kaiss91 <mboschferrete@gmail.com>
2024-11-13 12:56:02 +0100

    setting up for production dev

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from 'express'
+const express = require('express')
 
 const app = express()
 
@@ -9,4 +9,8 @@ app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
+})
+
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
 })
